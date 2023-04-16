9dec2bfb7813be5c3af500d7a46c3e32e7dc351f
Mertcan Taşkıran <65121494+mertcan-taskiran@users.noreply.github.com>
2023-04-16 16:23:48 +0300

    Initial Create

--- Shopping/Program.cs
+++ Shopping/Program.cs
@@ -16,7 +16,7 @@ namespace Shopping
                 options.UseSqlServer(connectionString));
             builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-            builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+            builder.Services.AddDefaultIdentity<IdentityUser>()
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             builder.Services.AddControllersWithViews();
 
